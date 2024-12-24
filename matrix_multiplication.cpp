743807bb2aebfc7efcd60a51b0151a1b45a6e960
fahim-haider <fahimhaider2@outlook.com>
2024-12-24 12:04:26 -0500

    Use threads to make RNG faster to fill matrices

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <thread>
 #include <fstream>
 #include <string>
 #include <random>
@@ -9,12 +9,14 @@
 using namespace std;
 
 // Stores the one dimensional size of the matrix
-const int MATRIXLENGTH = 500;
+const int MATRIXLENGTH = 1000;
 // Creates matrices of MATRIXLENGTH in length and width! Thus the size of the matrix is MATRIXLENGTH^2
 int matrix1[MATRIXLENGTH][MATRIXLENGTH];
 int matrix2[MATRIXLENGTH][MATRIXLENGTH];
 
 long dotProduct = 0;
+
+int THREADCOUNT = 4;
 
 // Create a random number generator with an upper and lower bound! (inclusive)
 int randomNumberGenerator(int lower_bound, int upper_bound) {
@@ -26,8 +28,14 @@ int randomNumberGenerator(int lower_bound, int upper_bound) {
     return distr(gen);
 }
 
-void fillMatrices() {
-
+void fillMatrices(int threadID) {
+    // Fill up the matrices!
+    for (int i = threadID - 1; i < MATRIXLENGTH; i += THREADCOUNT) {
+        for (int j = threadID - 1; j < MATRIXLENGTH; j += THREADCOUNT) {
+            matrix1[i][j] = randomNumberGenerator(0, 99);
+            matrix2[i][j] = randomNumberGenerator(0, 99);
+        }
+    }
 }
 
 int calculateProduct() {
@@ -46,7 +54,15 @@ int main (int argc, char* argv[]) {
     cout << "TEST" << endl;
 
     auto startFill = chrono::high_resolution_clock::now();
-    fillMatrices();
+    thread t1(fillMatrices, 1);
+    thread t2(fillMatrices, 2);
+    thread t3(fillMatrices, 3);
+    thread t4(fillMatrices, 4);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
     auto endFill = chrono::high_resolution_clock::now();
 
     chrono::duration<double> durationFill = endFill - startFill;
@@ -60,19 +76,7 @@ int main (int argc, char* argv[]) {
     chrono::duration<double> durationCalc = endCalc - startCalc;
 
 
-    cout << dotProduct << endl;
+    cout << "The dot product of the two randomly generated matrices is: " << dotProduct << endl;
     cout << "Time elapsed to fill: " << durationFill.count() << endl;
     cout << "Time elapsed to calculate: " << durationCalc.count() << endl;
-}
-void fillMatrices()
-{
-    // Fill up the matrices!
-    for (int i = 0; i < MATRIXLENGTH; i++)
-    {
-        for (int j = 0; j < MATRIXLENGTH; j++)
-        {
-            matrix1[i][j] = randomNumberGenerator(0, 99);
-            matrix2[i][j] = randomNumberGenerator(0, 99);
-        }
-    }
 }
