fe11a87c229af4cf2793405af4caf1c0861ef676
fahim-haider <fahimhaider2@outlook.com>
2024-12-24 21:44:23 -0500

    Add result documentation and simplify thread creation

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -5,18 +5,19 @@
 #include <random>
 #include <filesystem>
 #include <chrono>
+#include <windows.h>
 
 using namespace std;
 
 // Stores the one dimensional size of the matrix
 const int MATRIXLENGTH = 1000;
-// Creates matrices of MATRIXLENGTH in length and width! Thus the size of the matrix is MATRIXLENGTH^2
+// Creates matrices of MATRIXLENGTH in length and width!
+// Thus, the dimensions of the matrix is MATRIXLENGTH x MATRIXLENGTH
 int matrix1[MATRIXLENGTH][MATRIXLENGTH];
 int matrix2[MATRIXLENGTH][MATRIXLENGTH];
 
 long dotProduct = 0;
-
-int THREADCOUNT = 4;
+const int THREADCOUNT = 4;
 
 // Create a random number generator with an upper and lower bound! (inclusive)
 int randomNumberGenerator(int lower_bound, int upper_bound) {
@@ -30,8 +31,8 @@ int randomNumberGenerator(int lower_bound, int upper_bound) {
 
 void fillMatrices(int threadID) {
     // Fill up the matrices!
-    for (int i = threadID - 1; i < MATRIXLENGTH; i += THREADCOUNT) {
-        for (int j = threadID - 1; j < MATRIXLENGTH; j += THREADCOUNT) {
+    for (int i = threadID; i < MATRIXLENGTH; i += THREADCOUNT) {
+        for (int j = threadID; j < MATRIXLENGTH; j += THREADCOUNT) {
             matrix1[i][j] = randomNumberGenerator(0, 99);
             matrix2[i][j] = randomNumberGenerator(0, 99);
         }
@@ -50,24 +51,32 @@ int calculateProduct() {
     return 0;
 }
 
+HANDLE productLock;
 int main (int argc, char* argv[]) {
-    cout << "TEST" << endl;
+    //Creates output file
+    ofstream outFile("Results.txt");
+
+    productLock = CreateSemaphore(nullptr, 1, 1, nullptr);
+
+    if(productLock == nullptr) {
+        cerr << "CreateSemaphore error!" << GetLastError() << endl;
+        return 1;
+    }
+
+    thread threads[THREADCOUNT];
 
     auto startFill = chrono::high_resolution_clock::now();
-    thread t1(fillMatrices, 1);
-    thread t2(fillMatrices, 2);
-    thread t3(fillMatrices, 3);
-    thread t4(fillMatrices, 4);
+    for(int i = 0; i < THREADCOUNT; i ++) {
+        threads[i] = thread(fillMatrices, i);
+    }
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
+    for(auto& th: threads) {
+        th.join();
+    }
+
     auto endFill = chrono::high_resolution_clock::now();
 
     chrono::duration<double> durationFill = endFill - startFill;
-
-    cout << "FINISHED FILLING UP MATRICES" << endl;
 
     auto startCalc = chrono::high_resolution_clock::now();
     calculateProduct();
@@ -75,8 +84,8 @@ int main (int argc, char* argv[]) {
 
     chrono::duration<double> durationCalc = endCalc - startCalc;
 
-
-    cout << "The dot product of the two randomly generated matrices is: " << dotProduct << endl;
-    cout << "Time elapsed to fill: " << durationFill.count() << endl;
-    cout << "Time elapsed to calculate: " << durationCalc.count() << endl;
+    outFile << "Matrix size:                        " << MATRIXLENGTH << " x " << MATRIXLENGTH << endl;
+    outFile << "Dot Product:                        " << dotProduct << endl;
+    outFile << "Time elapsed to fill matrices:      " << durationFill.count() << endl;
+    outFile << "Time elapsed to calculate product:  " << durationCalc.count() << endl;
 }
